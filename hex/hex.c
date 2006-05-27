bd62fc474631a0102bd3672aff616ae2fae266d8
dhmunro <dhmunro>
2006-05-27 23:08:18 +0000

    fix mesh-orient bug

--- hex/hex.c
+++ hex/hex.c
@@ -1,5 +1,5 @@
 /*
- * $Id: hex.c,v 1.1 2005-09-18 22:05:47 dhmunro Exp $
+ * $Id: hex.c,v 1.2 2006-05-27 23:08:18 dhmunro Exp $
  * generic tools for dealing with a hex mesh
  */
 /* Copyright (c) 2005, The Regents of the University of California.
@@ -139,6 +139,7 @@ int hex_enter(HX_mesh *mesh, TK_ray *ray, long cell[],
 /* ------------------------------------------------------------------------ */
 
 /* enumerate all 24 orientations of a cube
+ * orientations[o][face] is mesh-face corresponding to xy-face face
  * - orientations[o][0] = o>>2 (==f0)
  * - orientations[o][2] = ((f0&4)?f0-4:f0+2) ^ ((o&2)?(f0&6)^6:0) ^ (o&1)
  * - that is, the 2 bit of o tells whether ijk cyclic order preserved
@@ -152,6 +153,17 @@ static int orientations[24][6]= {
   {4,5,0,1,2,3}, {4,5,1,0,3,2}, {4,5,2,3,1,0}, {4,5,3,2,0,1},
   {5,4,1,0,2,3}, {5,4,0,1,3,2}, {5,4,3,2,1,0}, {5,4,2,3,0,1}
 };
+
+/* orientations are mesh faces, here are corresponding mesh points
+static int orientpt[24][8] = {
+  {0,1,2,3,4,5,6,7}, {6,7,4,5,2,3,0,1}, {2,3,6,7,0,1,4,5}, {4,5,0,1,6,7,2,3},
+  {3,2,1,0,7,6,5,4}, {5,4,7,6,1,0,3,2}, {7,6,3,2,5,4,1,0}, {1,0,5,4,3,2,7,6},
+  {0,2,4,6,1,3,5,7}, {5,7,1,3,4,6,0,2}, {4,6,5,7,0,2,1,3}, {1,3,0,2,5,7,4,6},
+  {6,4,2,0,7,5,3,1}, {3,1,7,5,2,0,6,4}, {7,5,6,4,3,1,2,0}, {2,0,3,1,6,4,7,5},
+  {0,4,1,5,2,6,3,7}, {3,7,2,6,1,5,0,4}, {1,5,3,7,0,4,2,6}, {2,6,0,4,3,7,1,5},
+  {5,1,4,0,7,3,6,2}, {6,2,7,3,4,0,5,1}, {7,3,5,1,6,2,4,0}, {4,0,6,2,5,1,7,3}
+};
+*/
 
 static int orient_compose(int second, int first);
 static int orient_compose(int second, int first)
@@ -207,74 +219,87 @@ int hex_step(HX_mesh *mesh, long cell[], int face)
   return 0;
 }
 
+/* indices of points in xy on each face
+ * invert applied after face lookup
+ */
 static int faces[6][4]= {
   { 0, 2, 4, 6 }, { 1, 3, 5, 7 },
   { 0, 4, 1, 5 }, { 2, 6, 3, 7 },
   { 0, 1, 2, 3 }, { 4, 5, 6, 7 }};
 
-static int edges[6][3]= {
-  { 0, 1, 2 }, { 0, 1, 2 },
-  { 1, 2, 0 }, { 1, 2, 0 },
-  { 2, 0, 1 }, { 2, 0, 1 }};
+/* lo-valued face cyclically after a given face */
+static int loface[6] = { 2, 2, 4, 4, 0, 0 };
+
+/* im point order [dir1 reversed][dir2 reversed][point in xy face] */
+static int imorder[2][2][4] = {
+  { {0,1,2,3}, {2,3,0,1} }, { {1,0,3,2}, {3,2,1,0} }};
 
 void hex_face(HX_mesh *mesh, long cell, int face,
               TK_ray *ray, int invert, real xy[][3])
 {
-  real *p= ray->p;
-  real *qr= ray->qr;
-  int *order= ray->order;
+  real *p = ray->p;
+  real *qr = ray->qr;
+  int *order = ray->order;
+  real (*mxyz)[3] = mesh->xyz;
   int i, ix;
-  real (*mxyz)[3]= mesh->xyz;
-  int *fx= faces[(
-                  face= orientations[mesh->orient][face]
-                  )];
-  int *em= edges[face];
   long im[4];
-  long m= cell - mesh->stride[0] - mesh->stride[1] - mesh->stride[2];
-  if (face&1) m+= mesh->stride[em[0]];
-  mxyz= mxyz+m;  /* DEC cc doesn't like &mxyz[m] */
+  /* face (and invert) reference the xy array
+   * mface is corresponding face in mxyz
+   *   (mface&6), m1, m2 are mesh equivalents of lo-faces in cyclic order
+   */
+  int *fx = faces[face];
+  int mface = orientations[mesh->orient][face];
+  int m1 = orientations[mesh->orient][(face = loface[face])];
+  int m2 = orientations[mesh->orient][loface[face]];
+  int *morder = imorder[m1&1][m2&1];
+  long ms1 = mesh->stride[m1>>1];
+  long ms2 = mesh->stride[m2>>1];
+  long m = cell - mesh->stride[0] - mesh->stride[1] - mesh->stride[2];
+  if (mface&1) m += mesh->stride[mface>>1];
+  mxyz = mxyz+m;  /* DEC cc doesn't like &mxyz[m] */
 
-  im[0]= 0;
-  im[1]= mesh->stride[em[1]];
-  im[2]= mesh->stride[em[2]];
-  im[3]= im[1]+im[2];
+  im[morder[0]] = 0;
+  im[morder[1]] = ms1;
+  im[morder[2]] = ms2;
+  im[morder[3]] = ms1+ms2;
 
   for (i=0 ; i<4 ; i++) {
-    ix= fx[i]^invert;
-    m= im[i];
-    xy[ix][2]= mxyz[m][order[2]] - p[2];
-    xy[ix][1]= mxyz[m][order[1]] - xy[ix][2]*qr[1] - p[1];
-    xy[ix][0]= mxyz[m][order[0]] - xy[ix][2]*qr[0] - p[0];
+    ix = fx[i]^invert;
+    m = im[i];
+    xy[ix][2] = mxyz[m][order[2]] - p[2];
+    xy[ix][1] = mxyz[m][order[1]] - xy[ix][2]*qr[1] - p[1];
+    xy[ix][0] = mxyz[m][order[0]] - xy[ix][2]*qr[0] - p[0];
   }
 }
 
 void hex_edge(HX_mesh *mesh, long cell, int bndy, int face,
               TK_ray *ray, int invert, real xy[][3])
 {
-  real *p= ray->p;
-  real *qr= ray->qr;
-  int *order= ray->order;
-  real (*mxyz)[3]= mesh->xyz+cell; /* DEC cc doesn't like &mesh->xyz[cell] */
-  int f= edges[(
-                face= orientations[mesh->orient][face]
-                )][0];
-  int b= edges[bndy][0];
-  int fb= edges[face^bndy^6][0];
-  long m= mesh->stride[fb];
-  int ix, x= 0;
-  if (face&1) x+= 1<<f;
-  else        mxyz-= mesh->stride[f];
-  if (bndy&1) x+= 1<<b;
-  else        mxyz-= mesh->stride[b];
-  ix= x^invert;
-  xy[ix][2]= mxyz[-m][order[2]] - p[2];
-  xy[ix][1]= mxyz[-m][order[1]] - p[1] - xy[ix][2]*qr[1];
-  xy[ix][0]= mxyz[-m][order[0]] - p[0] - xy[ix][2]*qr[0];
-  x+= 1<<fb;
-  ix= x^invert;
-  xy[ix][2]= mxyz[0][order[2]] - p[2];
-  xy[ix][1]= mxyz[0][order[1]] - p[1] - xy[ix][2]*qr[1];
-  xy[ix][0]= mxyz[0][order[0]] - p[0] - xy[ix][2]*qr[0];
+  real *p = ray->p;
+  real *qr = ray->qr;
+  int *order = ray->order;
+  real (*mxyz)[3] = mesh->xyz+cell; /* DEC cc doesn't like &mesh->xyz[cell] */
+  int mface = orientations[mesh->orient][face];
+  int mbndy = orientations[mesh->orient][bndy];
+  long mm, mp, m = mesh->stride[(mface^mbndy^6)>>1];
+  int ix, x=0;
+  if (face&1) x += 1 << (face>>1);
+  if (!(mface&1)) mxyz -= mesh->stride[mface>>1];
+  if (bndy&1) x += 1 << (bndy>>1);
+  if (!(mbndy&1)) mxyz -= mesh->stride[mbndy>>1];
+  /* mesh->orient can reverse order of points on edge between xy, mxyz */
+  face ^= bndy^6;  /* a face perpendicular to this edge */
+  if ((orientations[mesh->orient][face] ^ face) & 1) mm = 0, mp = -m;
+  else mm = -m, mp = 0;
+  ix = x^invert;
+  xy[ix][2]= mxyz[mm][order[2]] - p[2];
+  xy[ix][1]= mxyz[mm][order[1]] - p[1] - xy[ix][2]*qr[1];
+  xy[ix][0]= mxyz[mm][order[0]] - p[0] - xy[ix][2]*qr[0];
+  x += 1 << (face>>1);
+  ix = x^invert;
+  xy[ix][2]= mxyz[mp][order[2]] - p[2];
+  xy[ix][1]= mxyz[mp][order[1]] - p[1] - xy[ix][2]*qr[1];
+  xy[ix][0]= mxyz[mp][order[0]] - p[0] - xy[ix][2]*qr[0];
 }
 
 static int triangle_flag= 0;
