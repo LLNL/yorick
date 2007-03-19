aff32f3bc777378790e965a32db05aee7235613b
dhmunro <dhmunro>
2007-03-19 16:04:09 +0000

    make emacs/yorick-auto.el recognize .i files as yorick mode

--- emacs/yorick-auto.el
+++ emacs/yorick-auto.el
@@ -5,3 +5,8 @@
 (autoload 'ysh "yorick" "see yorick.el in site-lisp dir" t)
 (autoload 'yssh "yorick" "see yorick.el in site-lisp dir" t)
 (autoload 'yutil-setup "yorick" "see yorick.el in site-lisp dir" nil)
+(if (assoc "\\.i$" auto-mode-alist)
+    ;; this presumes user doesn't want .i files to be in C mode (default)
+    ;; -- possibly this is antisocial, but an expert can easily put it back
+    (setcdr (assoc "\\.i$" auto-mode-alist) 'yorick-mode)
+  (setq auto-mode-alist (append '(("\\.i$" . yorick-mode)) auto-mode-alist)))
